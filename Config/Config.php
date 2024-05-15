38ecf62d4e9eed6b333c2f04dc2225ff516e03d7
ANDAKI85 <oscargutierrez10@gmail.com>
2024-05-15 16:14:28 -0500

    primer commit

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tiendaonline-/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tiendaonline";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+
+const USER_SMTP = "puntoeuniformes01@gmail.com";
+const PASS_SMTP = "Fabian.123";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
